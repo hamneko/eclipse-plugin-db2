67d90f2ca8ab913eeb1cafe00c11347a8b0b10b8
Jun Ishikawa <hamnyan@outlook.com>
2025-09-07 21:50:38 +0900

    #19 Resolve cursors

--- src/plugin/db2/DB2Language.java
+++ src/plugin/db2/DB2Language.java
@@ -1,6 +1,7 @@
 package plugin.db2;
 
 import org.eclipse.cdt.core.dom.ast.gnu.c.GCCLanguage;
+import org.eclipse.cdt.core.model.ICLanguageKeywords;
 import org.eclipse.cdt.core.parser.FileContent;
 import org.eclipse.cdt.core.parser.IParserLogService;
 import org.eclipse.cdt.core.parser.IScanner;
@@ -11,9 +12,21 @@ import plugin.db2.parser.scanner.DB2Scanner;
 
 public class DB2Language extends GCCLanguage {
 
+	public DB2Language() {
+	}
+
 	@Override
 	public String getId() {
 		return "plugin.db2.editor";
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getAdapter(Class<T> adapter) {
+		if (adapter.isAssignableFrom(ICLanguageKeywords.class)) {
+			return (T) this;
+		}
+		return super.getAdapter(adapter);
 	}
 
 	@Override
@@ -23,4 +36,13 @@ public class DB2Language extends GCCLanguage {
 				fcp);
 	}
 
+	@Override
+	public String[] getKeywords() {
+		String[] keywords = super.getKeywords();
+		String[] additionalKeywords = new String[] { "EXEC", "SQL" };
+		String[] result = new String[keywords.length + additionalKeywords.length];
+		System.arraycopy(keywords, 0, result, 0, keywords.length);
+		System.arraycopy(additionalKeywords, 0, result, keywords.length, additionalKeywords.length);
+		return result;
+	}
 }
